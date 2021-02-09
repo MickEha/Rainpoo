2075db7d973911b663abf719a8a27cebc4bab04d
MickEha <ehannomickael@gmail.com>
2021-02-09 10:04:45 +0100

    Agario 1

---
+++ p5/TP2/Player.py
@@ -0,0 +1,30 @@
+import random
+
+from pygame.math import Vector3, Vector2
+
+pass
+
+class Player:
+    def __init__(self):
+        self.vitesse=5
+        self.taille=2
+        self.forme= 2
+        self.couleur = Vector3()
+        self.position= Vector2 ()
+        self.direction= Vector2 ()
+        pass
+
+    def afficher (self):
+        pass
+
+
+    def manger(self):
+        pass
+
+
+    def mourir(self):
+        pass
+
+
+    def deplacer(self):
+        pass
---
+++ p5/TP2/main.py
@@ -0,0 +1,4 @@
+from TP2.Player import Player
+
+if __name__ == '__main__'():
+    mon_joueur = Player()
